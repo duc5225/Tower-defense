e6aba4f21ab8b281eaf70ff9b4bc473513017aaa
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2019-11-11 18:26:41 +0700

    -Add animation for NormalEnemy

---
+++ src/game/entity/GameEntity.java
@@ -0,0 +1,13 @@
+package game.entity;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public abstract class GameEntity {
+    protected Image image;
+    protected ImageView imageView;
+
+    public Image getImage() {
+        return image;
+    }
+}
